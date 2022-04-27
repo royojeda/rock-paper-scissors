05456a94a6219316c6c0a591df5f0596a79dff9a
Dennis Ojeda <royojeda1@gmail.com>
2022-04-27 15:07:53 +0800

    Add game winner announcement

--- script.js
+++ script.js
@@ -77,17 +77,29 @@ let currentScore;
 const buttons = document.querySelectorAll('button');
 let result = document.querySelector('#result');
 let score = document.querySelector('#score');
+let winner = document.querySelector('#winner');
 
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    playRound(button.id, computerPlay());
-    result.textContent = roundResult;
-    if (roundResult.includes('won')) {
-      playerScore++;
-    } else if (roundResult.includes('lost')) {
-      computerScore++;
-    }
-    currentScore = `Player ${playerScore} : ${computerScore} Computer`;
-    score.textContent = currentScore;
+  buttons.forEach((button) => {
+    button.addEventListener('click', function() {
+      play(button);
+    });
   });
-});
+
+function play(button) {
+  playRound(button.id, computerPlay());
+  result.textContent = roundResult;
+  if (roundResult.includes('won')) {
+    playerScore++;
+  } else if (roundResult.includes('lost')) {
+    computerScore++;
+  }
+  currentScore = `Player ${playerScore} : ${computerScore} Computer`;
+  score.textContent = currentScore;
+  if (playerScore === 5 || computerScore === 5) {
+    if (playerScore === 5) {
+      winner.textContent = 'Congratulations! You\'ve won the game!';
+    } else if (computerScore === 5) {
+      winner.textContent = 'Oh no! You\'ve lost the game. Try again!';
+    }
+  }
+}
